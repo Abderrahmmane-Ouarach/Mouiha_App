e92ffc3d9c92b8cd0457f75711586fa1e919531a
Othmane WINA <othmanwina@gmail.com>
2025-07-17 16:43:55 +0100

    fixing the navigation logic and adding the icons in the bottomTabNavigation; finishing the first version of the home page; including the logo ONEE in every page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,31 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './home/index'
-import QuizScreen from './quiz/index'
-import GamesScreen from './games/index'
-import StoriesScreen from './stories/index'
-import VideosScreen from './videos/index'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import GamesScreen from './games/index';
+import HomeScreen from './home/index';
+import QuizScreen from './quiz/index';
+import StoriesScreen from './stories/index';
+import VideosScreen from './videos/index';
 
 const Tab = createBottomTabNavigator();
 
 export default function AppTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === 'Home') iconName = 'home';
+          else if (route.name === 'Quiz') iconName = 'quiz';
+          else if (route.name === 'Games') iconName = 'sports-esports';
+          else if (route.name === 'Stories') iconName = 'menu-book';
+          else iconName = 'ondemand-video';
+          return <MaterialIcons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#007acc',
+        tabBarInactiveTintColor: '#888',
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Quiz" component={QuizScreen} />
       <Tab.Screen name="Games" component={GamesScreen} />
